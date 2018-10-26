09ab811cf5a39b3ae2e0c3c0eaf43e9cdae4de79
babinskajustyna <babinskajustynaa@gmail.com>
2018-10-26 10:27:48 +0200

    Initial commit

---
+++ src/app/Option.java
@@ -0,0 +1,43 @@
+package app;
+
+import java.util.NoSuchElementException;
+
+public enum Option {
+	EXIT(0, "Wyjœcie z programu"), 
+	ADD_BOOK(1, "Dodanie ksi¹¿ki"), 
+	ADD_MAGAZINE(2, "Dodanie magazynu/gazety"),
+	PRINT_BOOKS(3, "Wyœwietlenie dostêpnych ksi¹¿ek"), 
+	PRINT_MAGAZINES(4, "WYœwietlenie dostêpnych magazynów/gazet");
+
+	private int value;
+	private String description;
+
+	public int getValue() {
+		return value;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	Option(int value, String desc) {
+		this.value = value;
+		this.description = desc;
+	}
+
+	@Override
+	public String toString() {
+		return value + " " + description;
+	}
+
+	public static Option createFromInt(int option) throws NoSuchElementException {
+		Option result = null;
+		try {
+			result = Option.values()[option];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			throw new ArrayIndexOutOfBoundsException("Brak elementu o wskazanym ID");
+		}
+
+		return result;
+	}
+}
